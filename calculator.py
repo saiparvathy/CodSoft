ce52a64c241a965be5fba58c3a6743f153874fe9
saiparvathy <saiparvathi27@gmail.com>
2024-07-20 07:48:53 +0530

    second commit

--- calculator.py
+++ calculator.py
@@ -1,1 +1,23 @@
-print(a+b)
+def calculator():
+    num1= float(input("enter a number:"))
+    operator=input("enter an operator(+,-,*,/):")
+    num2=float(input("enter a number:"))
+
+    if operator== '+':
+        result=num1+num2
+    elif operator=='-':
+        result=num1-num2
+    elif operator=='*':
+        result=num1*num2
+    elif operator=='/':
+        if num2 !=0:
+            result=num1/num2
+        else:
+            result="invalid division"
+    else:
+        result="wrong operator"
+    print('Result:',result)
+calculator()
+
+
+
