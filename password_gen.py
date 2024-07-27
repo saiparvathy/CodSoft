2c2e6598ffc2c1e71bc1c0893df4c64e97f68712
saiparvathy <saiparvathi27@gmail.com>
2024-07-27 12:56:50 +0530

    second programm

---
+++ password_gen.py
@@ -0,0 +1,29 @@
+import random
+import string
+
+def generate_password_with_length(target_length):
+
+    uppercase_letters=string.ascii_uppercase
+    all_characters=string.ascii_letters+string.digits+string.punctuation
+
+    first_character=string.ascii_uppercase
+    remaining_length=target_length-1
+    remaining_password=''.joion(random.choice(all_characters) for _ in range(remaining_length))
+    random_password=first_character+remaining_password
+
+    return random_password
+def main():
+    target_length=input("Enter the length for password":)
+    if target_length.isdigit():
+        target_length=int(target_length)
+        if target_length <1:
+            print("password must be at leat 1")
+            return 
+    else:
+        print("invalid input please enter a positive integer.")
+        return 
+    password=generate_password_with_length
+
+    print("Generated password",password)
+    
+main()
